2afce74f25949d5de32b4f5f9603c2d353f92e66
fanntt <irfanariaputra@gmail.com>
2025-07-12 16:56:44 +0700

    update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:rumah_sakit_flutter_fanntt/model/rumahsakit_model.dart';
 
 class ApiService {
-  final String baseUrl = 'http://192.168.242.234:8000/api';
+  final String baseUrl = 'http://192.168.100.224:8000/api';
 
   Future<List<RumahSakit>> getAllRumahSakit() async {
     final response = await http.get(Uri.parse('$baseUrl/rumah-sakit'));
